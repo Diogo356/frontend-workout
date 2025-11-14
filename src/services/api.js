abdd29c93f334b152630558764b7fb8af5fc886a
Diogo356 <diogobelar262@gmail.com>
2025-11-14 01:10:17 -0300

    first commmit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import AuthService from './authService';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
